e9988bad99319102e8214adabc749fb013b3d888
vanphuc1208 <levanphucdn2003@gmail.com>
2023-10-05 16:11:28 +0700

    commit

--- SourceCode/Core/Src/main.c
+++ SourceCode/Core/Src/main.c
@@ -201,39 +201,45 @@ int main(void)
   HAL_TIM_Base_Start_IT (& htim2 );
   int led_idx=0;
   setTimer1(50);
+  setTimer2(100);
   while (1)
   {
     /* USER CODE END WHILE */
 	  if(timer1_flag==1) {
-		  setTimer1(50);
-		  if (led_idx==0) {
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,RESET);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin,SET);
-			  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin,SET);
-			  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin,SET);
+	 		  setTimer1(50);
+	 		  if (led_idx==0) {
+	 			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,RESET);
+	 			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin,SET);
+	 			  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin,SET);
+	 			  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin,SET);
 
-		  }
-		  else if (led_idx==1) {
-		  		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,SET);
-		  		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin,RESET);
-		  		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin,SET);
-		  		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin,SET);
-		  	}
-		  else if(led_idx==2) {
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,SET);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin,SET);
-			  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin,RESET);
-			  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin,SET);
-		  }
-		  else  {
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,SET);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin,SET);
-			  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin,SET);
-			  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin,RESET);
-		  }
-		  update7SEG(led_idx);
-		  if(led_idx>=3) led_idx=0;
-		  else led_idx++;
+	 		  }
+	 		  else if (led_idx==1) {
+	 		  		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,SET);
+	 		  		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin,RESET);
+	 		  		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin,SET);
+	 		  		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin,SET);
+	 		  	}
+	 		  else if(led_idx==2) {
+	 			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,SET);
+	 			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin,SET);
+	 			  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin,RESET);
+	 			  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin,SET);
+	 		  }
+	 		  else  {
+	 			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin,SET);
+	 			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin,SET);
+	 			  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin,SET);
+	 			  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin,RESET);
+	 		  }
+	 		  update7SEG(led_idx);
+
+	 		  if(led_idx>=3) led_idx=0;
+	 		  else led_idx++;
+	 	  }
+	  if(timer2_flag==1) {
+		  setTimer2(100);
+		  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 	  }
     /* USER CODE BEGIN 3 */
   }
@@ -334,17 +340,17 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_Pin|EN0_Pin|EN1_Pin|EN2_Pin
-                          |EN3_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, DOT_Pin|LED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, SEG0_Pin|SEG1_Pin|SEG2_Pin|SEG3_Pin
                           |SEG4_Pin|SEG5_Pin|SEG6_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_Pin EN0_Pin EN1_Pin EN2_Pin
-                           EN3_Pin */
-  GPIO_InitStruct.Pin = LED_Pin|EN0_Pin|EN1_Pin|EN2_Pin
-                          |EN3_Pin;
+  /*Configure GPIO pins : DOT_Pin LED_Pin EN0_Pin EN1_Pin
+                           EN2_Pin EN3_Pin */
+  GPIO_InitStruct.Pin = DOT_Pin|LED_Pin|EN0_Pin|EN1_Pin
+                          |EN2_Pin|EN3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
